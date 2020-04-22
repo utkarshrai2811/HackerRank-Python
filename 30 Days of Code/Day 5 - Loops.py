b2e83d29c97d7406245e727f7789964db744b7f2
Utkarsh Rai <utkarshrai.281100@gmail.com>
2020-04-22 09:37:04 +0530

    optimization

--- 30 Days of Code/Day 5 - Loops.py
+++ 30 Days of Code/Day 5 - Loops.py
@@ -2,9 +2,9 @@
 Objective
 In this challenge, we're going to use loops to help us do some simple math. Check out the Tutorial tab to learn more.
 Task
-Given an integer, N, print its first 10 multiples.
+Given an integer, n, print its first 10 multiples.
 Each multiple N * i (where 1 <= i <= 10) should be printed on a new line in the form:
-N x i = result.
+n x i = result.
 """
 
 
